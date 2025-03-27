4ed0e203021caf83822a8fb76ab0e9e6d2b671c2
M-Kwizera <m.kwizera@alustudent.com>
2025-03-27 13:46:24 +0200

    Houskeep

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,34 @@
-import React from "react"
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import WorkoutPlans from './pages/WorkoutPlans';
-
-// eslint-disable-next-line no-unused-vars
-
-
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-
+import React, { useState } from "react";
+import bcrypt from "bcryptjs";
 
 function App() {
+  const [password, setPassword] = useState("");
+  
+  // You need to make the function async to use await inside it
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Store user in DB (assuming a User model)
+    // This would be an API call or some other action
+    console.log("Hashed Password:", hashedPassword);
+  };
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/workout-plans" element={<WorkoutPlans />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="password" 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)} 
+          placeholder="Enter password" 
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 }
-
-// Registration route
-// app.post('/api/register', async (req, res) => {
-//   const { username, password } = req.body;
-  
-  // Hash the password
-  const hashedPassword = await bcrypt.hash(password, 10);
-  
-  // Store user in DB (assuming a User model)
-  // Example: await User.create({ username, password: hashedPassword });
-
-  // Send confirmation response
-  res.status(201).json({ message: 'User registered successfully' });
-
 
 export default App;
